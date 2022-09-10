689f5480632cde7927e4b8e6a428617bbe44f36c
sstollenwerk <sstollenwerk@yahoo.co.nz>
2022-09-10 13:25:42 +1200

    Do day 16 part 2

--- src/day16.py
+++ src/day16.py
@@ -1,4 +1,4 @@
-from itertools import cycle
+from itertools import cycle, accumulate
 from operator import mul
 from collections.abc import Iterable
 
@@ -15,6 +15,8 @@ from generic import (
 
 Signal = list[int]
 
+def replicate(n,i):
+    return (i for _ in range(n))
 
 def parse(s: str) -> Signal:
     return list(map(int, s))
@@ -28,7 +30,7 @@ def part(sig: Signal, n: int) -> int:
 
 def make_pattern(n: int) -> Iterable[int]:
     base = [0, 1, 0, -1]
-    r = cycle(flatten([i] * n for i in base))
+    r = cycle(flatten(replicate(n,i) for i in base))
     next(r)
     return r
 
@@ -37,14 +39,29 @@ def step(s: Signal) -> Signal:
     return [part(s, i + 1) for i in range(len(s))]
 
 
+
 def part1(s):
     signal = parse(s)
     res = nth(iterate(step, signal), 100)
-    return int("".join(map(str, res))[:8])
+    return int(as_str(res)[:8])
 
+def as_str(s:Signal) -> str:
+    return "".join(map(str, s))
+
+def last_digits(signal:Signal) -> Signal:
+    part = accumulate(signal[::-1])
+    return [abs(n)%10 for n in list(part)[::-1]]
+    
 
 def part2(s):
-    pass
+    # needed to look up help for this one - https://www.reddit.com/r/adventofcode/comments/ebf5cy/comment/fb4bvw4/
+    # disappointing that this doesn't work where offset is in first half of signal.
+    signal = parse(s*10000)
+    offset = int(as_str(signal)[:7])
+    signal = signal[offset:]
+    res = nth(iterate(last_digits, signal), 100)
+    print(res[:100])
+    return int(as_str(res)[0:0+8])
 
 
 def main():
